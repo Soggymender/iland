71dd6a2aaefa97826b3e62d020d5eb358af51d90
unknown <rabblerooster@gmail.com>
2020-02-06 18:31:38 -0600

    ClipTest

--- src/test/java/org/engine/anim/info/ClipTest.java
+++ src/test/java/org/engine/anim/info/ClipTest.java
@@ -1,12 +1,12 @@
 package org.engine.anim.info;
 
 import org.engine.anim.info.Clip;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class ClipTest {
 
     @Test
-    public void buildClip() throws Exception {
+    public void buildClipTest() {
         Clip clip = new Clip();
         clip.name = "Test Clip";
     }
